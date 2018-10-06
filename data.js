68ea4922a0b1b77535029751a57830a710cc7a6a
miguel <miguelp@csedge.org>
2018-10-06 13:18:58 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,22 @@
 // Example
-let message = "Hello World!";
-console.log(message);
-
-// Type your code below!
+let message = "MiGeRu!";
+letsole.log(message);
+let month=2;
+let day=1;
+let year=2003;
+let animal="otter"
+let hoby="skateBoarding"
+let name="miguel"
+let likeWearingHoodies = true;
+let likeSkateingBoarding = true;
+let age;
+console. log(month)
+console. log(day)
+console. log(year)
+console. log(animal)
+console. log(hoby)
+console. log(name)
+console. log(likeWearingHoodies)
+console. log(likeSkateingBoarding)
+console. log(age)
+// Type your code below!e
